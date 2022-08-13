99860f680a910c0989695e79f946d886edbabab8
Seth Wooten <slwooten14@gmail.com>
2022-08-13 12:10:22 -0400

    added addClass mut to typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -30,6 +30,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    addClass(className: String!, startDate: String!, endDate: String!, description: String!): Class
   }
 `;
 
