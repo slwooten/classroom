01c1763db279d459604cc2de33686d8045b62500
Seth Wooten <slwooten14@gmail.com>
2022-08-25 13:04:49 -0400

    minimal styling on coursPage

--- client/src/pages/CoursePage.js
+++ client/src/pages/CoursePage.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Box, Container, Typography, Card, Button } from '@mui/material';
 import { useQuery, useMutation } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 
@@ -10,7 +11,7 @@ import StudentModal from '../components/StudentModal';
 const CoursePage = () => {
 
   let { courseId } = useParams();
- 
+
   /// STUDENT FORM STATE for STUDENT FORM ///
   const [formState, setFormState] = useState({ firstName: '', lastName: '' });
 
@@ -22,7 +23,7 @@ const CoursePage = () => {
 
   /// ADD STUDENT MUTATION ///
   const [addStudent, { error, studentData }] = useMutation(ADD_STUDENT);
- 
+
   /// HANDLE STUDENT FORM CHANGE ///
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -61,7 +62,7 @@ const CoursePage = () => {
       lastName: '',
     });
   };
-  
+
   if (loading) {
     return (
       <div>Loading course, please wait...</div>
@@ -69,46 +70,74 @@ const CoursePage = () => {
   };
 
   return (
-    <>
-      <div>
-        Hey
-        <h1>{courseInfo?.courseName}</h1>
-        <h2>Start Date: {courseInfo?.startDate}</h2>
-        <h2>End Date: {courseInfo?.endDate}</h2>
-        <h2>Number of Students: {courseInfo?.studentCount}</h2>
-        <h2>Description:</h2>
-        <p>{courseInfo?.description}</p>
-      </div>
-      <div>
-        {courseInfo?.students.map((student) => (
-          <StudentModal key={student._id} student={student} />
-        ))}
-      </div>
-      <div>
-        <form onSubmit={handleFormSubmit}>
-          <input
-            type='text'
-            placeholder='Student first name'
-            name='firstName'
-            value={formState.firstName}
-            onChange={handleChange}
-          />
-          <input
-            type='text'
-            placeholder='Student last name'
-            name='lastName'
-            value={formState.lastName}
-            onChange={handleChange}
-          />
-          <button type='submit'>Add Student</button>
-        </form>
-      </div>
-      {error && (
-        <div>
-          {error.message}
-        </div>
-      )}
-    </>
+    <main>
+      <Box>
+        <Container>
+          <Typography variant='h4' align='center' sx={{ mt: 8, mb: 4 }}>{courseInfo?.courseName}</Typography>
+          <Typography variant='h6' align='center' sx={{ m: 2 }}>Number of Students: {courseInfo?.studentCount}</Typography>
+          <Typography variant='h6' align='center'>Start/End Dates:</Typography>
+          <Typography variant='subtitle2' align='center'>{courseInfo?.startDate} - {courseInfo?.endDate}</Typography>
+        </Container>
+        <Container
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexWrap: 'wrap',
+          }}
+        >
+          <Card variant='outlined' sx={{
+            p: 2,
+            m: 3,
+          }}>
+            <Typography variant='h6'>Description:</Typography>
+            <Typography variant='subtitle1'>{courseInfo?.description}</Typography>
+          </Card>
+          <Card variant='outlined' sx={{
+            p: 2,
+            m: 3,
+          }}>
+            <Typography variant='h6'>Students:</Typography>
+            {courseInfo?.students.map((student) => (
+              <StudentModal key={student._id} student={student} />
+            ))}
+          </Card>
+          <Card variant='outlined' sx={{
+            maxWidth: 600,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            p: 2,
+            flex: 'auto'
+          }}>
+            <form onSubmit={handleFormSubmit}>
+              <input
+                type='text'
+                placeholder='Student first name'
+                name='firstName'
+                value={formState.firstName}
+                onChange={handleChange}
+              />
+              <input
+                type='text'
+                placeholder='Student last name'
+                name='lastName'
+                value={formState.lastName}
+                onChange={handleChange}
+              />
+              <button type='submit'>Add Student</button>
+            </form>
+          </Card>
+        </Container>
+        {error && (
+          <div>
+            {error.message}
+          </div>
+        )}
+      </Box>
+    </main>
   )
 }
 
