6fbb1f52df19a3a0e6956565d0e09235cb1a0f64
Seth Wooten <slwooten14@gmail.com>
2022-08-30 17:09:41 -0400

    added env variables

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const secret = 'mysecretshhhhhh';
+const secret = process.env.JWT_SECRET;
 const expiration = '1h';
 
 module.exports = {
